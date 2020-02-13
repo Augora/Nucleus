2d627b4ebd283c8f54a8324e1deaea85dc923177
kevinbacas <bacas.kevin@hotmail.fr>
2020-02-13 19:02:10 +0100

    Add age to depute object

--- src/Mappings/Depute.ts
+++ src/Mappings/Depute.ts
@@ -24,6 +24,7 @@ export function MapDepute(depute: Types.External.NosDeputesFR.Depute) {
     NombreMandats: depute.nb_mandats,
     Twitter: depute.twitter,
     EstEnMandat: depute.ancien_depute !== 1,
+    Age: parseInt(moment(depute.date_naissance).fromNow(true)),
     SitesWeb: depute.sites_web.map(s => s.site),
     Emails: depute.emails.map(e => e.email),
     Adresses: depute.adresses.map(a => a.adresse),
