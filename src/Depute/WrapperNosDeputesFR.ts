fe7b3ecff7c82947e7685b57799bfd5818f537eb
kevinbacas <bacas.kevin@hotmail.fr>
2020-03-23 20:48:32 +0100

    Add retry on GetDeputeFromNosDeputesFR function

--- src/Depute/WrapperNosDeputesFR.ts
+++ src/Depute/WrapperNosDeputesFR.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { from } from 'rxjs'
-import { mergeMap, toArray } from 'rxjs/operators'
+import { mergeMap, toArray, retry } from 'rxjs/operators'
 
 import { GetLogger } from '../Common/Logger'
 
@@ -24,8 +24,10 @@ export function GetDeputesBySlugFromNosDeputesFR(
 ): Promise<Types.External.NosDeputesFR.Depute[]> {
   GetLogger().info('GetDeputesBySlugFromNosDeputesFR', slugs)
   return from(slugs)
-    .pipe(mergeMap(val => GetDeputeFromNosDeputesFR(val), 1))
-    .pipe(toArray())
+    .pipe(
+      mergeMap(val => GetDeputeFromNosDeputesFR(val), 1),
+      toArray()
+    )
     .toPromise()
 }
 
@@ -33,13 +35,17 @@ export function GetDeputeFromNosDeputesFR(
   slug: string
 ): Promise<Types.External.NosDeputesFR.Depute> {
   GetLogger().info('GetDeputeFromNosDeputesFR', slug)
-  return axios
-    .get<Types.External.NosDeputesFR.DeputeWrapper>(
-      `https://www.nosdeputes.fr/${slug}/json`
-    )
-    .then(res => {
-      GetLogger().info(`Retrieved ${slug} from nosdeputes.fr.`)
-      return res
-    })
-    .then(res => res.data.depute)
+  return from(
+    axios
+      .get<Types.External.NosDeputesFR.DeputeWrapper>(
+        `https://www.nosdeputes.fr/${slug}/json`
+      )
+      .then(res => {
+        GetLogger().info(`Retrieved ${slug} from nosdeputes.fr.`)
+        return res
+      })
+      .then(res => res.data.depute)
+  )
+    .pipe(retry(3))
+    .toPromise()
 }
