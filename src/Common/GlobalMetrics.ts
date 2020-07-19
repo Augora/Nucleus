1e932b19ab3d3bcebafe6cd1a4c0054ca27218d7
kevinbacas <bacas.kevin@hotmail.fr>
2020-07-19 16:05:09 +0200

    Fix single class per file error

--- src/Common/GlobalMetrics.ts
+++ src/Common/GlobalMetrics.ts
@@ -1,3 +1,5 @@
+import Metric from './Metric'
+
 class GlobalMetrics {
   deputes: Metric = new Metric()
 
@@ -20,15 +22,6 @@ class GlobalMetrics {
   addErroredDepute() {
     this.deputes.error = this.deputes.error + 1
   }
-}
-
-class Metric {
-  created: number = 0
-  read: number = 0
-  updated: number = 0
-  removed: number = 0
-  none: number = 0
-  error: number = 0
 }
 
 export default new GlobalMetrics()
