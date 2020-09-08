2721866b2188b3c698a835275134bdb083c14980
kevinbacas <bacas.kevin@hotmail.fr>
2020-09-08 19:11:33 +0200

    Slack webhook URL from env

--- src/Common/SlackWrapper.ts
+++ src/Common/SlackWrapper.ts
@@ -1,58 +1,49 @@
 import axios from 'axios'
 
-export function SendNewDeputeNotification(
-  depute: Types.Canonical.Depute,
-  sigle: string
-) {
-  return axios.post(
-    'https://hooks.slack.com/services/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',
-    {
-      blocks: [
-        {
-          type: 'section',
-          text: {
+export function SendNewDeputeNotification(depute: Types.Canonical.Depute) {
+  return axios.post(process.env.SLACK_WEBHOOK_URL, {
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `<https://augora.fr/depute/${depute.Slug}|${depute.Nom}>, député ${depute.GroupeParlementaire.Sigle} a été ajouté en base.`,
+        },
+      },
+      {
+        type: 'context',
+        elements: [
+          {
             type: 'mrkdwn',
-            text: `<https://augora.fr/depute/${depute.Slug}|${depute.Nom}>, député ${sigle} a été ajouté en base.`,
+            text: 'From Nucleus with :heart:',
           },
-        },
-        {
-          type: 'context',
-          elements: [
-            {
-              type: 'mrkdwn',
-              text: 'From Nucleus with :heart:',
-            },
-          ],
-        },
-      ],
-    }
-  )
+        ],
+      },
+    ],
+  })
 }
 
 export function SendNewGroupeParlementaireNotification(
   groupe: Types.Canonical.GroupeParlementaire
 ) {
-  return axios.post(
-    'https://hooks.slack.com/services/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',
-    {
-      blocks: [
-        {
-          type: 'section',
-          text: {
+  return axios.post(process.env.SLACK_WEBHOOK_URL, {
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Le groupe ${groupe.NomComplet} a été ajouté en base.`,
+        },
+      },
+      {
+        type: 'context',
+        elements: [
+          {
             type: 'mrkdwn',
-            text: `Le groupe ${groupe.NomComplet} a été ajouté en base.`,
+            text: 'From Nucleus with :heart:',
           },
-        },
-        {
-          type: 'context',
-          elements: [
-            {
-              type: 'mrkdwn',
-              text: 'From Nucleus with :heart:',
-            },
-          ],
-        },
-      ],
-    }
-  )
+        ],
+      },
+    ],
+  })
 }
