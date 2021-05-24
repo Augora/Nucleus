a721c7cae2f27cb75b57ae1b0427bc831dc4aef3
KevinBacas <bacas.kevin@hotmail.fr>
2021-05-24 12:42:39 +0200

    Add cmd line args + update github workflow

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,49 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+const optionDefinitions = [
+  { name: 'groupes', alias: 'g', type: Boolean },
+  { name: 'activites', alias: 'a', type: Boolean },
+  { name: 'deputes', alias: 'd', type: Boolean },
+]
+
+import commandLineArgs from 'command-line-args'
+const options = commandLineArgs(optionDefinitions)
+
 import { ManageGroupes } from './GroupeParlementaire/Manager'
 import { ManageDeputes } from './Depute/Manager'
 import { ManageActivites } from './Activite/Manager'
 import { GetLogger } from './Common/Logger'
 
-ManageGroupes()
-  .then(() => {
-    console.log('Imported groupes')
-  })
-  .catch((err) => {
-    GetLogger().error(err)
-    throw err
-  })
+if (options.groupes) {
+  ManageGroupes()
+    .then(() => {
+      console.log('Imported groupes')
+    })
+    .catch((err) => {
+      GetLogger().error(err)
+      throw err
+    })
+}
 
-ManageDeputes()
-  .then(() => {
-    console.log('Imported deputes')
-  })
-  .catch((err) => {
-    GetLogger().error(err)
-    throw err
-  })
+if (options.deputes) {
+  ManageDeputes()
+    .then(() => {
+      console.log('Imported deputes')
+    })
+    .catch((err) => {
+      GetLogger().error(err)
+      throw err
+    })
+}
 
-ManageActivites()
-  .then(() => {
-    console.log('Imported activites')
-  })
-  .catch((err) => {
-    GetLogger().error(err)
-    throw err
-  })
+if (options.activites) {
+  ManageActivites()
+    .then(() => {
+      console.log('Imported activites')
+    })
+    .catch((err) => {
+      GetLogger().error(err)
+      throw err
+    })
+}
