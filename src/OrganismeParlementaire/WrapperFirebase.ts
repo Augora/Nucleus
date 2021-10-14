3a87eeba74be14c1f174c60738ec44f79141ca33
KevinBacas <bacas.kevin@hotmail.fr>
2021-10-14 11:59:23 +0200

    Fixed errors by replacing let to const

--- src/OrganismeParlementaire/WrapperFirebase.ts
+++ src/OrganismeParlementaire/WrapperFirebase.ts
@@ -5,7 +5,7 @@ import isNull from 'lodash/isNull'
 import isObject from 'lodash/isObject'
 
 function removeEmpty(obj) {
-  let newObj = {}
+  const newObj = {}
   Object.keys(obj).forEach((key) => {
     if (obj[key] === Object(obj[key])) newObj[key] = removeEmpty(obj[key])
     else if (obj[key] !== undefined) newObj[key] = obj[key]
