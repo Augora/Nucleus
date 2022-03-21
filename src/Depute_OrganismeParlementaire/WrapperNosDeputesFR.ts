83dc7e5bf824de3a123205818d41515025fef904
KevinBacas <bacas.kevin@hotmail.fr>
2022-03-21 18:44:48 +0100

    Fix for missing organisme informations

--- src/Depute_OrganismeParlementaire/WrapperNosDeputesFR.ts
+++ src/Depute_OrganismeParlementaire/WrapperNosDeputesFR.ts
@@ -11,5 +11,11 @@ export function GetDeputesInOrganisme(
     .get<Types.External.NosDeputesFR.DeputesWrapper>(
       `https://www.nosdeputes.fr/organisme/${organismeSlug}/json`
     )
-    .then((res) => res.data.deputes.map((d) => d.depute))
+    .then((res) => {
+      if (res.data && res.data.deputes) {
+        return res.data.deputes.map((d) => d.depute)
+      } else {
+        return []
+      }
+    })
 }
