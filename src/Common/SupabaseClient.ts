282c45090d17dcebc04c7045d798fda6f8001be2
KevinBacas <bacas.kevin@hotmail.fr>
2022-11-03 17:29:08 +0100

    Remove forced bug on Supabase auth

--- src/Common/SupabaseClient.ts
+++ src/Common/SupabaseClient.ts
@@ -3,5 +3,5 @@ import { Database } from '../../Types/database.types'
 
 export default createClient<Database>(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY + ',dnjsqd'
+  process.env.SUPABASE_KEY
 )
